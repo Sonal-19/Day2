f551c41b3b7c69fe637447fd6850925c5d2990c7
unknown <sonalchy02@gmail.com>
2022-07-18 12:00:31 +0530

    first commit

---
+++ operators.py
@@ -0,0 +1,103 @@
+"""Arithematic operator"""
+# a = 15
+# b = 3
+# print(a + b)    #addition 0f two numbers
+# print(a-b)      #subtracton 0f two numbers
+# print(a*b)      #multiplication 0f two numbers
+# print(a/b)      #division 0f two numbers
+# print(a%b)      #modulus 0f two numbers
+# print(a//b)     #do 0f two numbers
+# print(a**b)     #do 0f two numbers
+
+
+"""Comparision/Relational Operators"""
+# a = 67
+# b = 45
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+# print(a>=b)
+# print(a==b)
+# print(a!=b)
+
+
+"""Logical operators"""
+# a = 9
+# b = 5
+# print(a>b and b>a)
+# print(a>b and b<a)
+# print(a>b or b>a)
+# print(a<b or a<b)
+# print(not(b>a))
+
+"""Assignment operators"""
+# a = 7
+# a += 3
+# print(a)
+
+# a = 7
+# a -= 3
+# print(a)
+
+# a = 7
+# a *= 3
+# print(a)
+
+# a = 7
+# a /= 3
+# print(a)
+
+# a = 7
+# a %= 3
+# print(a)
+
+# a = 7
+# a //= 3
+# print(a)
+
+# a = 7
+# a **= 3
+# print(a)
+
+# a = 7
+# a >>= 3
+# print(a)
+
+# a = 7
+# a <<= 3
+# print(a)
+
+
+"""Bitwise Operator"""
+# a = 4
+# b = 5 
+# print("a & b =", a & b)     #bitwise and operation
+# print("a | b =", a | b)     #bitwise or operation
+# print (" ~b =", ~b)         #bitwise not operation
+# print("a ^ b =", a ^ b)     #bitwise xor operation
+
+# a = 10
+# b = 2
+# print("a >> b =", a >> b)      #bitwise right side operation
+# print("a << b =", a << b)      ##bitwise left side operation
+
+
+"""identity operator"""
+# a = 10
+# b = 10
+# print(a is b)           #is operation
+# print(a is not b)       #is not operation
+# a = 10 
+# b = 20
+# print(a is b)           #is operation
+# print(a is not b)       #is not operation
+
+
+"""Membership operator"""
+# x = ["apple", "banana"]
+# print("banana" in x)            #Membership in operator
+# print("banana" not in x)        #Membership not in operator
+
+# x = ["5", "9"]
+# print("9" in x)                 #Membership in operator
+# print("9" not in x)             #Membership not in operator
